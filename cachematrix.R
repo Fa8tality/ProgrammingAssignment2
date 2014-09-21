39e1ca4f22ba1ed30064a3e1bbf351679f88ff5f
Fa8tality <st1323@att.com>
2014-09-21 11:14:09 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,15 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-##he first function, makeVector creates a special "vector", which is really a list containing a function to
-
-##    set the value of the vector
-##    get the value of the vector
-##    set the value of the mean
-##    get the value of the mean
-
-
-
-
+## R Programming Assignment 2: Caching the Inverse of a Matrix
+##
+## The first function: makeCacheMatrix function, creates a special "matrix",
+## which is really a list containing a function to
+##   1. set the value of the matrix
+##   2. get the value of the matrix
+##   3. set the value of the inverse of the matrix
+##   4. get the value of the inverse of the matrix
+##
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
 	set <- function(y) {
@@ -26,14 +24,12 @@ makeCacheMatrix <- function(x = matrix()) {
         getInverseMatrix=getInverseMatrix)
 }
 
-
-## Write a short comment describing this function
-
-##The following function calculates the mean of the special "vector" created with the above function. 
-##However, it first checks to see if the mean has already been calculated. 
-##If so, it gets the mean from the cache and skips the computation. 
-##Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
-
+## The second function: cacheSolve function, calculates the inverse of the special "matrix"
+## from the makeCacheMatrix function.  However, it first checks to see if the inverse has already
+## been calculated.  If so, it gets the inverse from the cache and skips the computation.
+## Otherwise, it calculates the inverse of the matrix and sets the value of the inverse
+## in the cache via the setinverseMatrix function.
+##
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 	m <- x$getInverseMatrix()
